88741608a948acdcb52ccb14b90b4b05823ba9df
Snizhana <s29870@pjwstk.edu.pl>
2025-01-12 15:29:03 +0100

    add SpringBootApplication file in module 'valorant-data'

---
+++ valorant-data/src/main/java/com/project/ValorantDataApplication.java
@@ -0,0 +1,10 @@
+package com.project;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValorantDataApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}

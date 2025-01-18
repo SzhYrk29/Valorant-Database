612088dccc29c0bad53812b1bbeb866631918fc6
Snizhana <s29870@pjwstk.edu.pl>
2025-01-18 21:59:20 +0100

    add methods in repositories for services

--- valorant-data/src/main/java/com/project/repository/AgentRepository.java
+++ valorant-data/src/main/java/com/project/repository/AgentRepository.java
@@ -4,4 +4,7 @@ import com.project.model.Agent;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AgentRepository extends JpaRepository<Agent, String> {
+    Agent findByUuid(String uuid);
+    Agent findByDisplayName(String displayName);
+    void deleteByUuid(String uuid);
 }
--- valorant-data/src/main/java/com/project/repository/MapRepository.java
+++ valorant-data/src/main/java/com/project/repository/MapRepository.java
@@ -1,7 +1,10 @@
 package com.project.repository;
 
 import com.project.model.Map;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MapRepository extends CrudRepository<Map, String> {
+public interface MapRepository extends JpaRepository<Map, String> {
+    Map findByUuid(String uuid);
+    Map findByDisplayName(String displayName);
+    void deleteByUuid(String uuid);
 }

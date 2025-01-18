f87765b898bf1b9350fb7ecde5c52063c6d79f9e
Snizhana <s29870@pjwstk.edu.pl>
2025-01-18 17:14:45 +0100

    technical commit

--- valorant-data/src/main/java/com/project/ValorantDataApplication.java
+++ valorant-data/src/main/java/com/project/ValorantDataApplication.java
@@ -1,10 +1,11 @@
 package com.project;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ValorantDataApplication {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        SpringApplication.run(ValorantDataApplication.class, args);
     }
 }

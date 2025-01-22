7d3f0e207127f3eb7f40978ab0d4613cf076e0da
Snizhana <s29870@pjwstk.edu.pl>
2025-01-22 23:17:46 +0100

    add logging of invoking endpoints

--- valorant-api/src/main/java/com/project/controller/AgentController.java
+++ valorant-api/src/main/java/com/project/controller/AgentController.java
@@ -2,6 +2,7 @@ package com.project.controller;
 
 import com.project.model.Agent;
 import com.project.service.AgentService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 
 public class AgentController {
@@ -22,21 +24,25 @@ public class AgentController {
 
     @GetMapping("api/agents")
     public ResponseEntity<List<Agent>> getAllAgents() {
+        log.info("Endpoint 'api/agents' was invoked and method 'getAllAgents' was executed.");
         return new ResponseEntity<>(agentService.getAllAgents(), HttpStatus.OK);
     }
 
     @GetMapping("api/agents/uuid/{uuid}")
     public ResponseEntity<Agent> getAgentByUuid(@PathVariable("uuid") String uuid) {
+        log.info("Endpoint 'api/agents/uuid' was invoked and method 'getAgentByUuid' was executed.");
         return new ResponseEntity<>(agentService.getAgentByUuid(uuid), HttpStatus.OK);
     }
 
     @GetMapping("api/agents/name/{displayName}")
     public ResponseEntity<Agent> getAgentByDisplayName(@PathVariable("displayName") String displayName) {
+        log.info("Endpoint 'api/agents/name' was invoked and method 'getAgentByDisplayName' was executed.");
         return new ResponseEntity<>(agentService.getAgentByDisplayName(displayName), HttpStatus.OK);
     }
 
     @DeleteMapping("api/agents/delete/{uuid}")
     public ResponseEntity<Void> deleteAgentByUuid(@PathVariable("uuid") String uuid) {
+        log.info("Endpoint 'api/agents/delete' was invoked and method 'deleteAgentByUuid' was executed.");
         agentService.deleteAgentByUuid(uuid);
         return new ResponseEntity<>(HttpStatus.OK);
     }
--- valorant-api/src/main/java/com/project/controller/MapController.java
+++ valorant-api/src/main/java/com/project/controller/MapController.java
@@ -2,6 +2,7 @@ package com.project.controller;
 
 import com.project.model.Map;
 import com.project.service.MapService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 public class MapController {
 
@@ -22,21 +24,25 @@ public class MapController {
 
     @GetMapping("api/maps")
     public ResponseEntity<List<Map>> getAllMaps() {
+        log.info("Endpoint 'api/maps' was invoked and method 'getAllMaps' was executed.");
         return new ResponseEntity<>(mapService.getAllMaps(), HttpStatus.OK);
     }
 
     @GetMapping("api/maps/uuid/{uuid}")
     public ResponseEntity<Map> getMapByUuid(@PathVariable("uuid") String uuid) {
+        log.info("Endpoint 'api/maps/uuid' was invoked and method 'getMapByUuid' was executed.");
         return new ResponseEntity<>(mapService.getMapByUuid(uuid), HttpStatus.OK);
     }
 
     @GetMapping("api/maps/name/{displayName}")
     public ResponseEntity<Map> getMapByDisplayName(@PathVariable("displayName") String displayName) {
+        log.info("Endpoint 'api/maps/name' was invoked and method 'getMapByDisplayName' was executed.");
         return new ResponseEntity<>(mapService.getMapByDisplayName(displayName), HttpStatus.OK);
     }
 
     @DeleteMapping("api/maps/delete/{uuid}")
     public ResponseEntity<Void> deleteMapByUuid(@PathVariable("uuid") String uuid) {
+        log.info("Endpoint 'api/maps/delete' was invoked and method 'deleteMapByUuid' was executed.");
         this.mapService.deleteMapByUuid(uuid);
         return new ResponseEntity<>(HttpStatus.OK);
     }
